81ff1df21e11f7680b22396b87b85a815ffe3629
nitro2 <ngothanhnhannt@gmail.com>
2022-11-28 23:56:31 +0700

    Support 2 threads

--- main.cpp
+++ main.cpp
@@ -1,19 +1,52 @@
-#include "SFML/Window.hpp"
+#include <iostream>
+
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+void renderingThread(sf::Window* window)
+{
+    // activate the window's context
+    window->setActive(true);
+
+    // the rendering loop
+    while (window->isOpen())
+    {
+        // draw...
+
+        // end the current frame -- this is a rendering function (it requires the context to be active)
+        window->display();
+    }
+}
+
 
 int main()
 {
-    sf::Window window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(1200, 1200), "Lines 98");
+    window.setVerticalSyncEnabled(true);
+    // deactivate its context
+    window.setActive(false);
 
+    // launch the rendering thread
+    sf::Thread thread(&renderingThread, &window);
+    thread.launch();
+    
     // run the program as long as the window is open
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
+// check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
             // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
+            switch(event.type)
+            {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
